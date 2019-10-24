579ccfb5ab090339a42676c8f62b8e2f60623b3b
RunziQiang <50156805+RunziQiang@users.noreply.github.com>
2019-10-24 14:02:47 -0400

    Update split_folder.py

--- lib/split_folder.py
+++ lib/split_folder.py
@@ -5,7 +5,7 @@ import pandas as pd
 folder_path = "/home/lingyi/Desktop/repo/Fall2019-proj3-sec1--proj3-sec1-grp3/data"
 train_path = "/home/lingyi/Desktop/repo/Fall2019-proj3-sec1--proj3-sec1-grp3/data/train"
 
-images = [f for f in sorted(os.listdir(train_path))]
+images = [f for f in sorted(os.listdir(train_path)) if f.endswith(".jpg")]
 
 test_images = list( images[i] for i in range(0, 2500, 10))
 
